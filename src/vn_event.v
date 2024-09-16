a5c715893e0a4e735e97e72e5f300e4ba4b63be9
Jacob Davis <ismyhc@gmail.com>
2024-09-16 14:59:23 -0400

    stringify

--- src/vn_event.v
+++ src/vn_event.v
@@ -4,15 +4,19 @@ import json
 
 pub struct VNEvent {
 pub:
-	id         string
-	pubkey     string
-	created_at u64
-	kind       u16
-	tags       [][]string
-	content    string
-	sig        string
+	id         string     @[required]
+	pubkey     string     @[required]
+	created_at u64        @[required]
+	kind       u16        @[required]
+	tags       [][]string @[required]
+	content    string     @[required]
+	sig        string     @[required]
 }
 
 pub fn (e &VNEvent) event_relay_message(subscription_id string) string {
 	return '["EVENT", "${subscription_id}", ' + json.encode(e) + ']'
+}
+
+pub fn (e &VNEvent) stringify() string {
+	return json.encode(e)
 }
--- v.mod
+++ v.mod
@@ -1,7 +1,7 @@
 Module {
 	name: 'vnostr'
 	description: 'Core Nostr Types and functions'
-	version: '0.1.2'
+	version: '0.1.3'
 	license: 'MIT'
 	dependencies: ['ismyhc.vsecp256k1', 'ismyhc.vbech32']
 }
